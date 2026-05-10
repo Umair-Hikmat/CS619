08cc2d52148b63320fa852e2ab55b3ee74b43963
Umair-Hikmat <umairhikmat@gmail.com>
2026-05-10 13:52:30 +0500

    Update app.py

--- app.py
+++ app.py
@@ -734,7 +734,7 @@ else:
     
         else:
             st.warning("Record ID not available in dataset")
-        records_df = db.get_records(patient_id)
+        records_df = db.get_all_medical_records_by_doctor(doctor_id)
         
         if not records_df.empty:
         
