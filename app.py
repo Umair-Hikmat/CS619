da5d536adbf4b940031cbb3582ae1df7cd65e0f1
Umair-Hikmat <umairhikmat@gmail.com>
2026-05-10 14:18:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -599,7 +599,7 @@ else:
         # =====================================================
         # MEDICAL RECORDS
         # =====================================================
-      elif menu == "Medical Records":
+    elif menu == "Medical Records":
     
         st.title("📋 Medical Records")
     
